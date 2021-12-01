55dc354789c6d7682eb38b4f4a129c267b94c5c0
Ivan Vasquez <ivanvasquezp@outlook.com>
2021-12-01 09:12:20 -0700

    Dependencies update
    - Dependency update
    - Fixes for new dependencies

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var express = require('express');
-var app = express();
-var exphbs  = require('express-handlebars');
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var path = require('path');
+const express = require('express');
+const engine  = require('express-handlebars');
+const http = require('http');
+const path = require('path');
+const app = express();
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 app.use('/public', express.static(path.join(__dirname + '/public')));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
 
 app.get('/chat/:id', function(req, res){	
 	res.render('index', {
@@ -15,67 +19,33 @@ app.get('/chat/:id', function(req, res){
 	});
 });
 
-io.on('connection', function(socket){
-	socket.on('message', function(data){						
-		socket.broadcast.to(data.room).emit('message', data);		
-	});
-	socket.on('login', function(room){				
-		socket.join(room);				
-		io.sockets.in(room).emit('user:on', {room: room, length: io.sockets.adapter.rooms[room].length});
-	});
-	socket.on('idle', function(){				
-		Object.keys(io.sockets.adapter.rooms).forEach(function(room) {		
-			if(io.sockets.adapter.rooms[room].length > 1){
-				socket.broadcast.to(room).emit('user:away', room);
+io.on('connection', (socket) => {
+	socket.on('message', (data) => {
+		socket.broadcast.to(data.room).emit('message', data);				
+	});	
+	socket.on('login', (room) => {				
+		socket.join(room);					
+		io.sockets.in(room).emit('user:on', {room: room, length: io.sockets.adapter.rooms.get(room).size});		
+	});  
+	socket.on('idle', () => {						
+		io.sockets.adapter.rooms.forEach((room, key) => {			
+			if(room.size > 1){
+				socket.broadcast.to(key).emit('user:away', key);				
 			}
 		});
-	});
-	socket.on('active', function(){
-
-		Object.keys(io.sockets.adapter.rooms).forEach(function(room) {					
-			socket.broadcast.to(room).emit('user:on', {room: room, length: io.sockets.adapter.rooms[room].length});
+	});	
+	socket.on('active', () => {
+		io.sockets.adapter.rooms.forEach((room, key) => {			
+			socket.broadcast.to(key).emit('user:on', {room: key, length: io.sockets.adapter.rooms.get(key).size});
 		});
 	});
-	socket.on('disconnect', function(){				
-		Object.keys(io.sockets.adapter.rooms).forEach(function(room) {					
-			socket.broadcast.to(room).emit('user:off', room);
+	socket.on('disconnect', () => {						
+		io.sockets.adapter.rooms.forEach((room, key) => {			
+			socket.broadcast.to(key).emit('user:off', key);
 		});
 	});
-
 });
 
-/*
-/*.forEach(function(room){
-			io.in(room).emit('user:off', {id: socket.id});
-		});
-
-
-io.sockets.once('connection', function(socket){
-	socket.on('login', function(room){ 
-		socket.join(room);
-		io.sockets.in(room).emit('message', {
-			type: 'status',
-			text: 'Is now connected',
-			created: Date.now(),
-			username: socket.request.user.username
-		});
-
-		socket.on('message', function(data){				
-			io.sockets.in(data.room).emit('message', data);		
-		});
-
-		socket.on('disconnect', function () { 
-			io.sockets.in(room).emit({ 
-				type: 'status',
-				text: 'disconnected',
-				created: Date.now(),
-				username: socket.request.user.username
-			});  
-		});
-	});
-});*/
-
-
-http.listen(3000, function(){
-	console.log('listening on *:3000');
-});
+server.listen(3000, () => {
+	console.log('server is running on port', server.address().port);
+})
